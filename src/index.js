b623a8020e0a8f1d612b0b9b6de0338035a86a3a
Pezu-git <artem_nikolaev69@mail.ru>
2021-05-02 14:32:04 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,38 +1,28 @@
+/* eslint-disable max-len */
 /* eslint-disable no-restricted-syntax */
+import './style.css';
 
-export default class Team {
-  constructor() {
-    this.members = [
-      {
-        name: 'Лучник',
-        type: 'Bowman',
-        health: 50,
-        level: 1,
-        attack: 40,
-        defence: 10,
-      },
-      {
-        name: 'Мечник',
-        type: 'Swordsman',
-        health: 70,
-        level: 1,
-        attack: 30,
-        defence: 20,
-      },
-      {
-        name: 'Маг',
-        type: 'Magician',
-        health: 40,
-        level: 1,
-        attack: 50,
-        defence: 10,
-      },
-    ];
+// eslint-disable-next-line eol-last
+const goblin = document.querySelectorAll('.goblin');
+const min = Math.ceil(0);
+const max = Math.floor(16);
+const numArr = [];
+
+function appear() {
+  const active = document.querySelector('.active');
+  const number = Math.floor(Math.random() * (max - min));
+  if (numArr[0] === number && number > min) { // если число повторяется и может быть равно Max
+    goblin[number - 1].classList.add('active');
+    numArr[0] = number - 1;
+  } else if (numArr[0] === number && number < max - 1) { // если число повторяется и может быть равно Min
+    goblin[number + 1].classList.add('active');
+    numArr[0] = number + 1;
+  } else {
+    goblin[number].classList.add('active');
+    numArr[0] = number;
   }
-
-  * [Symbol.iterator]() {
-    for (const person of this.members) {
-      yield person;
-    }
+  if (active) {
+    active.classList.remove('active');
   }
 }
+setInterval(() => appear(), 500);
--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,13 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+/* eslint-disable camelcase */
+/* eslint-disable linebreak-style */
+/* eslint-disable import/no-unresolved */
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+__webpack_base_uri__ = 'http://localhost:8080';
 
 module.exports = {
   entry: './src/index.js',
@@ -12,14 +20,40 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [{
+          loader: 'file-loader',
+        }],
+      },
     ],
   },
   plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
   ],
 };
