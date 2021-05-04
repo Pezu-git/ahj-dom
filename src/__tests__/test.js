87bc25e70827d0d3d11bdb728c213ece05704be3
Pezu-git <artem_nikolaev69@mail.ru>
2021-05-04 08:16:30 +0300

    fixed

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -5,10 +5,21 @@
 import appear from '../js/app.js';
 
 test('waits 1 second before ending the game', () => {
-  document.body.innerHTML =
-    '<div>' +
-    '  <img class="goblin" />' +
-    '</div>';
+  function template() {
+    return `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+<div class="goblin"></div>
+</body>
+</html>`;
+  }
+  template();
   appear();
   expect(appear).toBe('goblin active');
   // const expected = goblin[10].classList.contains('active');
--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,44 @@
 /* eslint-disable linebreak-style */
-/* eslint-disable max-len */
-/* eslint-disable no-restricted-syntax */
-// eslint-disable-next-line eol-last
+/* eslint-disable no-plusplus */
+/* eslint-disable linebreak-style */
 
-const numArr = [];
+const board = document.createElement('div');
+board.classList.add('board');
+document.body.appendChild(board);
+
+for (let i = 0; i < 16; i++) {
+  const newMap = document.createElement('div');
+  newMap.classList.add('map');
+  board.appendChild(newMap);
+  const newImgDiv = document.createElement('div');
+  newImgDiv.classList.add('img');
+  newMap.appendChild(newImgDiv);
+  const imgDiv = new Image();
+  imgDiv.src = 'https://github.com/netology-code/ahj-homeworks/blob/simplification/dom/pic/goblin.png?raw=true';
+  imgDiv.classList.add('goblin');
+  newImgDiv.appendChild(imgDiv);
+}
 export default function appear() {
+  const numArr = [];
   const min = Math.ceil(0);
   const max = Math.floor(16);
   const number = Math.floor(Math.random() * (max - min));
   const goblin = document.querySelectorAll('.goblin');
   const active = document.querySelector('.active');
-  // const number = Math.floor(Math.random() * (max - min));
-  if (numArr[0] === number && number > min) { // если число повторяется и может быть равно Max
+
+  while (numArr[0] === number && number > min) {
     goblin[number - 1].classList.add('active');
     numArr[0] = number - 1;
-  } else if (numArr[0] === number && number < max - 1) { // если число повторяется и может быть равно Min
+  }
+  while (numArr[0] === number && number < max - 1) {
     goblin[number + 1].classList.add('active');
     numArr[0] = number + 1;
-  } else {
-    goblin[number].classList.add('active');
-    numArr[0] = number;
   }
+  goblin[number].classList.add('active');
+  numArr[0] = number;
+
   if (active) {
     active.classList.remove('active');
   }
 }
-setInterval(() => appear(), 500);
+setInterval(() => appear(), 1000);
